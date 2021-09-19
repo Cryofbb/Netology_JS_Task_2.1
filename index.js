dff6b5b4ddb0c8656d69948cba96e93b7d1641fd
Cryofbb <78804095+Cryofbb@users.noreply.github.com>
2021-09-19 13:29:19 +0000

    v2

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
-const arr = []
+const salaryArray = []
 const taxArray = []
-let salarySumm = 0
 
 for (let i = 0; i < 70; i++) {
   if (i < 5){
-    arr.push(10000);
-  }
+    salaryArray.push(10000)}
   if (i > 4 && i < 20){
-    arr.push(25000);
-  }
+    salaryArray.push(25000)}
   if (i > 19 && i < 60){
-    arr.push(50000);
-  }
+    salaryArray.push(50000)}
   if (i > 59 && i < 70){
-    arr.push(100000);
-  }
-}
-function getTax(taxPercent){
-  for (let i = 0; i < arr.length; i++) {
-    const arrSalary = arr[i];
-    let tax = arrSalary * taxPercent / 100;
-    taxArray.push(tax); 
-    salarySumm+=taxArray[i];
-  }
-}
-function getMin(tax){
-  return minTax=Math.min(...arr)
-}
-function getMax(tax){
-  return maxTax=Math.max(...arr)
-}
-function getStat(tax){
-  getTax(tax)
-  let salaryStat = {
-    midTax : `Средние налоговые отчисления с человека составили ${Math.floor(salarySumm / taxArray.length)} руб.`,
-    sumTax : `Суммарные налоговые отчисления составили ${salarySumm} руб.`,
-    maxSalary : `Максимальная зарплата составляет ${getMax(tax)} руб., сумма налога составляет ${getMax(tax) * tax /100} руб, эффективная ставка ${(getMax(tax) * tax) / getMax(tax)}%`,
-    minSalary : `Минимальная зарплата составляет ${getMin(tax)} руб., сумма налога составляет ${getMin(tax) * tax /100} руб, эффективная ставка ${(getMin(tax) * tax) / getMin(tax)}%`
-  }
-  console.log(salaryStat)
+    salaryArray.push(100000)}
 }
 
-getStat(prompt("Введите налоговую ставку"));
+function getStatTax(salaryArray, rate){
+  let sum = 0
+  for (let i = 0; i < salaryArray.length; i++) {
+    taxArray.push(salaryArray[i] * rate / 100)
+    sum += taxArray[i];
+  }
+  
+  let max = {}
+  max.salary = Math.max(...salaryArray);
+  max.tax = max.salary * rate / 100;
+  max.stake = max.tax / max.salary * 100;
+
+  let min = {}
+  min.salary = Math.min(...salaryArray);
+  min.tax = min.salary * rate / 100;
+  min.stake = min.tax / min.salary * 100;
+  
+  let tax = {}
+  tax.sum = sum;
+  tax.person = Math.floor(sum / salaryArray.length);
+  tax.min = min;
+  tax.max = max;
+  return tax
+}
+
+let stat = getStatTax(salaryArray, 20);
+
+console.log(`Средние налоговые отчисления с человека составили ${stat.person} руб.`)
+console.log(`Суммарные налоговые отчисления составили ${stat.sum} руб.`)
+console.log(`Максимальная зарплата составляет ${stat.max.salary} руб., сумма налога составляет ${stat.max.tax} руб, эффективная ставка ${stat.max.stake}%`)
+console.log(`Минимальная зарплата составляет ${stat.min.salary} руб., сумма налога составляет ${stat.min.tax} руб, эффективная ставка ${stat.min.stake}%`)
